ebde2cf8c0e1d6efe6debd1ca8b26ba9f38ffb3b
Masterchef365 <duncan.freeman1@gmail.com>
2024-08-30 19:05:33 -0700

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+use serde::{de::DeserializeOwned, Serialize};
+use tarpc::Transport;
+use bincode::Error as BincodeError;
+use std::convert::Infallible;
+use futures_util::sink::SinkExt;
+use futures_util::stream::StreamExt;
+
+pub fn bincode_stream<Item, SinkItem, S>(
+    sock: S,
+) -> impl Transport<Item, SinkItem, TransportError = RpcError>
+where
+    SinkItem: DeserializeOwned,
+    Item: Serialize,
+    S: Transport<Vec<u8>, Vec<u8>>,
+    RpcError: From<S::Error>,
+{
+    sock.with(|client_msg| async move { Ok(bincode::serialize(&client_msg)?) })
+        .map(|byt| Ok(bincode::deserialize(&byt?)?))
+}
+
+#[derive(thiserror::Error, Debug)]
+pub enum RpcError {
+    #[error("Serialization")]
+    Bincode(#[from] BincodeError),
+    #[error("Websocket")]
+    WebSocket(#[from] Infallible),
+}
